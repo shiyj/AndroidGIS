1c9925582d1a35cabf7d1142f86add2f72b4142c
shiyj <shiyj.cn@gmail.com>
2013-01-15 23:28:58 +0800

    change ui

---
+++ gen/com/spatialite/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.spatialite;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/com/spatialite/MapCanvas.java
+++ src/com/spatialite/MapCanvas.java
@@ -8,7 +8,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import jsqlite.Callback;
+import jsqlite.Stmt;
 import jsqlite.Exception;
+//引入JTS
+import com.vividsolutions.jts.geom.*;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKBReader;
+
+import android.R.integer;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -20,42 +27,69 @@ import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MapCanvas extends Activity {
+	
 	private static final String TAG = TestActivity.class.getName();
-
+	private final jsqlite.Database mDatabase;
+	private GeometryCollection mGeometryCollection;
+	private Envelope mGeomEnvelope;
+	private MyView mView;
+	public MapCanvas()throws Exception {
+		mDatabase = new jsqlite.Database();
+		mDatabase.open("/mnt/sdcard/test-2.3.sqlite", jsqlite.Constants.SQLITE_OPEN_READONLY);
+	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		MyView mv = new MyView(this);
-		setContentView(mv);
-
+		Toast.makeText(getApplicationContext(), "正在加载数据库……",
+				Toast.LENGTH_SHORT).show();
+		mView = new MyView(this);
+		setContentView(mView);
 	}
-
-	private class GeoPoint {
-		float x, y;
-		String name;
-	}
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+    	 if (keyCode == KeyEvent.KEYCODE_VOLUME_UP){
+    		 mView.zoomIn();
+    		 return true;
+    	 } else  if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN){
+    		 mView.zoomOut();
+    		 return true;
+    	 }
+         return super.onKeyDown(keyCode, event);
+    }
 
 	public class MyView extends View {
 		/**
-		 * 分别表示：SEL选择属性 DRAG 拖动 ZOOM 放大缩小
+		 * 分别表示：SELECT选择属性 DRAG 拖动 ZOOM 放大缩小
 		 */
 		private static final int NONE = 0;
 		private static final int ZOOM = 1;
+		private static final int DRAG = 2;
+		private static final int SELECT = 3;
+		
+		private Paint paint = new Paint();
 		// 设置默认模式
-		private int m_nMode = NONE;
-		PointF m_pStart = new PointF();
-		PointF m_pMid = new PointF();
-		float m_fOldDist = 1f;
-		private ArrayList<GeoPoint> pointArrays = new ArrayList<GeoPoint>();
-		private float rate;
-		private float leavel = 1;
-		int w =300;
-		int h = 400;
+		private int m_nMode = DRAG;
+		//触摸标记
+		private double m_startX=0;
+		private double m_startY;
+		private double m_endX;
+		private double m_endY;
+		private double m_fOldDist = 1f;
+		
+		private double rate;//全屏时比例
+		private double leavel = 1;
+		//相对于原数据坐标的平移位置dx,dy。
+		private double dx=0;
+		private double dy=0;
+		int view_w =300;
+		int view_h = 400;
 		MyView(Context context) {
 			super(context);
 			getData();
@@ -64,182 +98,280 @@ public class MapCanvas extends Activity {
 		@Override
 		protected void onDraw(Canvas canvas) {
 			super.onDraw(canvas);
-			w = getWidth();
-			h = getHeight();
-			Paint paint = new Paint();
-			float rate1 = w / h;
-			if (rate1 > (1308585.40 - 319224.01) / (5214373.42 / 3934674.16))
-				rate = (float) (h / (5214373.42 / 3934674.16));
-			else
-				rate = (float) (w / (1308585.40 - 319224.01));
+			view_w=getWidth();
+			view_h=getHeight();
+			
 			paint.setColor(Color.BLUE);
 			paint.setStyle(Style.FILL);
-			canvas.drawRect(new Rect(0, 0, w, h), paint);
-
-			paint.setColor(Color.GREEN);
-			canvas.drawText("Hello", 10, 50, paint);
-
-			paint.setColor(Color.RED);
-			paint.setStyle(Style.FILL);
-			int len = pointArrays.size();
-			for (int i = 0; i < len; i++) {
-				canvas.drawCircle(transData(pointArrays.get(i).x, 0), transData(pointArrays.get(i).y, 1), 10, paint);
+			//canvas.drawRect(new Rect(0, 0, view_w, view_h), paint);
+			if(null==mGeometryCollection)
+			{
+				return;
 			}
+			DrawGeomtry(canvas,mGeometryCollection);
 		}
 
 		@Override
 		public boolean onTouchEvent(MotionEvent event) {
 			switch (event.getAction() & MotionEvent.ACTION_MASK) {
+			
 			case MotionEvent.ACTION_DOWN:
-				float x = event.getX();
-				float y = event.getY();
-				// 测试数据为读取出来存放在arraylist中，下一步改成直接从数据库中查询。
-				Iterator<GeoPoint> it = pointArrays.iterator();
-				while (it.hasNext()) {
-					GeoPoint tmp = it.next();
-					float tmpx, tmpy;
-					tmpx = transData(tmp.x, 0);
-					tmpy = transData(tmp.y, 1);
-					float distan = ((x - tmpx) * (x - tmpx) + (y + tmpy - getHeight())
-							* (y + tmpy - getHeight()));
-					if (distan <= 200) {
-
-						Toast toast = Toast.makeText(getApplicationContext(),
-								tmp.name, Toast.LENGTH_SHORT);
-						toast.setGravity(Gravity.LEFT | Gravity.TOP,
-								(int) (x + 5), (int) (y + 5));
-						toast.show();
-					}
-				}
+				//触摸获取位置查询
+				m_endX = m_startX= event.getX();
+				m_endY = m_startY = event.getY();
+				query(m_startX,m_startY);
+				break;
+			case MotionEvent.ACTION_UP:
+				m_endX = m_startX= event.getX();
+				m_endY = m_startY = event.getY();
 				break;
 			case MotionEvent.ACTION_POINTER_1_DOWN:
+				//计算两点的距离，大于10f才认为是两根手指。
 				m_fOldDist = spacing(event);
-				if (m_fOldDist > 10f) {
+				if (m_fOldDist > 10f) 
+				{
 					m_nMode = ZOOM;
 				}
 				break;
-			case MotionEvent.ACTION_UP:
 			case MotionEvent.ACTION_POINTER_1_UP:
-				m_nMode = NONE;
+				//中途其中一根手指离开屏幕，退出缩放模式。
+				//同时将开始标记点记为当前。
+				m_nMode = DRAG;
+				m_endX = m_startX= event.getX();
+				m_endY = m_startY = event.getY();
 				break;
 			case MotionEvent.ACTION_MOVE:
-				if (m_nMode == ZOOM) {
-					float a_fNewDist = spacing(event);
-					if (a_fNewDist - m_fOldDist > 30
-							|| a_fNewDist - m_fOldDist < -30) {
+				m_endX = event.getX();
+				m_endY = event.getY();
+				if (m_nMode == ZOOM) 
+				{
+					double a_fNewDist = spacing(event);
+					if (a_fNewDist - m_fOldDist > 14 
+							|| a_fNewDist - m_fOldDist < -14){
 						zoom(a_fNewDist-m_fOldDist);
+						m_fOldDist = a_fNewDist;
 					}
 				}
-				else
-					drag();
+				else if(m_nMode == DRAG)
+				{
+					if(m_endX-m_startX >10 || m_endX-m_startX < -10
+							|| m_endY - m_startY > 10
+							|| m_endY - m_startY < -10) {
+						drag();
+						m_endX = m_startX= event.getX();
+						m_endY = m_startY = event.getY();
+					}
+				}
 				postInvalidate();
 				break;
 			}
 			return true;
 		}
-		private void zoom(float dis){
-			float len=(float) Math.sqrt(w*w+h*h);
-			leavel+=dis/(len*5);
-			if(leavel<0.5)
-				leavel= (float) 0.5;
+		private void zoom(double dis){
+			double min_leavel = rate/2;
+			if(leavel<min_leavel){
+				leavel=  min_leavel;
+				return;
+			}
+			double len = Math.sqrt(view_w*view_w+view_h*view_h);
+			double originx,originy;
+			originx = transData(view_w/2, 0);
+			originy = transData(view_h/2, 1);
+			leavel = (1+dis/len)*leavel;
+			m_endX=antTransData(originx, 0);
+			m_endY=antTransData(originy, 1);
+			m_startX = view_w/2;
+			m_startY = view_h/2;
+			drag();
+			m_endX = m_startX = view_w/2;
+			m_endY = m_startY = view_h/2;
+		}
+		public void zoomIn() {
+			
+			double originx,originy;
+			originx = transData(view_w/2, 0);
+			originy = transData(view_h/2, 1);
+			leavel += 0.2*leavel;
+			m_endX=antTransData(originx, 0);
+			m_endY=antTransData(originy, 1);
+			m_startX = view_w/2;
+			m_startY = view_h/2;
+			drag();
+			m_endX = m_startX = view_w/2;
+			m_endY = m_startY = view_h/2;
+			postInvalidate();
+		}
+		public void zoomOut() {
+			double originx,originy;
+			originx = transData(view_w/2, 0);
+			originy = transData(view_h/2, 1);
+			leavel -= 0.2*leavel;
+			m_endX=antTransData(originx, 0);
+			m_endY=antTransData(originy, 1);
+			m_startX = view_w/2;
+			m_startY = view_h/2;
+			drag();
+			m_endX = m_startX = view_w/2;
+			m_endY = m_startY = view_h/2;
+			postInvalidate();
 		}
 		private void drag(){
+			double tmp_dx=0;//直接赋值给dx会影响dy的判断
+			if (m_endX - m_startX > 10 || m_endX - m_startX < -10) {
+				tmp_dx = antTransData(m_endX, 0) - antTransData(m_startX, 0);
+			}
+			if (m_endY - m_startY > 10 || m_endY - m_startY < -10) {
+				dy += (antTransData(m_endY, 1) - antTransData(m_startY, 1));
+			}
+			dx += tmp_dx;
+			//不能超出地图范围。
+			double width = mGeomEnvelope.getWidth();
+			double height = mGeomEnvelope.getHeight();
+			if(dx > width){
+				dx = width;
+			}
+			else if(dx < -width){
+				dx = -width;
+			}
+			if(dy > height){
+				dy = height;
+			}
+			else if (dy < -height) {
+				dy = -height;
+			}
+		}
+		private void query(double x,double y){
 			
 		}
-		
 		/**
-		 * 将投影坐标转换成屏幕坐标。
+		 * 将数据坐标转换成屏幕坐标。
 		 * @param f 坐标值
-		 * @param i 0表示x，1表示y
+		 * @param i 0表示x，1表示y;
 		 * @return
 		 */
-		private float transData(float f, int i) {
-			float result = 0;
-			if (i == 0)
-				result = (float) ((f - 319224.61) * rate);
-			else
-				result = (float) (h - ((f - 3934674.16) * rate));
+		private double transData(double f, double i) {
+			double result = 0;
+			if (i == 0)	{
+				double minx;
+				minx = mGeomEnvelope.getMinX();
+				result = (f + dx - minx)*leavel;
+			}
+			else {
+				double miny;
+				miny = mGeomEnvelope.getMinY();
+				result = (view_h - (f + dy - miny)*leavel);
+			}
+			return result;
+		}
+		/**
+		 * 将屏幕坐标转换成数据坐标。
+		 * @param f 坐标值
+		 * @param i 0表示x，1表示y;
+		 * @return
+		 */
+		private double antTransData(double f, double i) {
+			double result = 0;
+			if (i == 0)	{
+				double minx;
+				minx = mGeomEnvelope.getMinX();
+				result = (f/leavel + minx - dx);
+			}
+			else {
+				double miny;
+				miny = mGeomEnvelope.getMinY();
+				result = ((view_h -f)/leavel + miny - dy);
+			}
+			return result;
+		}
+		
 
-			return result*leavel;
+		//计算触摸两个点的距离。
+		private double spacing(MotionEvent event) {
+
+			double x = event.getX(0) - event.getX(1);
+			double y = event.getY(0) - event.getY(1);
+			return  Math.sqrt(x * x + y * y);
 		}
 
-		private float spacing(MotionEvent event) {
-
-			float x = event.getX(0) - event.getX(1);
-			float y = event.getY(0) - event.getY(1);
-			return (float) Math.sqrt(x * x + y * y);
+		private void DrawGeomtry(Canvas canvas,GeometryCollection geomCollection)
+		{			
+			String type= geomCollection.getGeometryType();
+			if(type.equals("MultiPoint"))
+			{
+				MultiPoint mulPoint=(MultiPoint)geomCollection;
+				DrawPoint(canvas,mulPoint);
+			}
 		}
+		private void DrawPoint(Canvas canvas,MultiPoint mulPoint)
+		{
+			paint.setColor(Color.GREEN);
+			canvas.drawText("Hello", 10, 50, paint);
 
+			paint.setColor(Color.RED);
+			paint.setStyle(Style.FILL);
+			
+			int len = mulPoint.getNumGeometries();
+			for (int i = 0; i < len; i++) {
+				Point pt = (Point)mulPoint.getGeometryN(i);
+				canvas.drawCircle((float)transData(pt.getX(), 0), (float)transData(pt.getY(), 1), 10, paint);
+			}
+		}
 		private void getData() {
 			try {
-				Class.forName("SQLite.JDBCDriver").newInstance();
-				jsqlite.Database db = new jsqlite.Database();
-				// 检测、创建数据库的文件夹
-				File dir = new File("/mnt/sdcard/");
-				if (!dir.exists()) {
-					dir.mkdir();
+				// Create query
+				//String query = "SELECT name, AsBinary(ST_Transform(geometry,4326)) from Towns where peoples > 350000";
+				String query = "SELECT name, AsBinary(ST_Transform(geometry,4326)) from Towns";
+				Stmt stmt = mDatabase.prepare(query);
+				ArrayList<Geometry> geoms = new ArrayList<Geometry>();
+				//geoms = new Geometry[colum_count];
+				GeometryFactory geomFactory = new GeometryFactory();
+				while(stmt.step()) {
+					// Create JTS geometry from binary representation
+					// returned from database
+					try {
+						geoms.add(new WKBReader().read(stmt.column_bytes(1)));
+						Geometry ge= new WKBReader().read(stmt.column_bytes(1));
+						String type =ge.getGeometryType();
+						type.equals("Point");
+					} catch (ParseException e) {
+						Log.e(TAG, e.getMessage());
+					}
 				}
-				// 如果文件夹已经存在了
+				Point[] arrGeom = (Point[])geoms.toArray(new Point[geoms.size()]);//new Geometry[geoms.size()];			
+				mGeometryCollection = geomFactory.createMultiPoint(arrGeom);
+				mGeomEnvelope = mGeometryCollection.getEnvelopeInternal();
+				double geom_width,geom_height;
+				geom_width = mGeomEnvelope.getWidth();
+				geom_height = mGeomEnvelope.getHeight();
+				//只有一个点的图层，原比例无缩放。
+				if(geom_height<1e-8 && geom_width <1e-8){
+					rate = 1;
+					mGeomEnvelope.init(view_w,0,view_h,0);
+				}
 				else {
-					// 检查文件是否存在
-					dir = new File("/mnt/sdcard/", "test-2.3.sqlite");
-					if (!dir.exists()) {
-						Log.v(TAG, "不存在地理数据库！");
-						Toast.makeText(getApplicationContext(), "不存在数据库",
-								Toast.LENGTH_SHORT).show();
-						return;
+					double rate1 = view_h/view_w;
+					double minx,miny,maxx,maxy;
+					minx = mGeomEnvelope.getMinX();
+					miny = mGeomEnvelope.getMinY();
+					maxx = mGeomEnvelope.getMaxX();
+					maxy = mGeomEnvelope.getMaxY();
+					//屏幕比外接矩形窄
+					if(rate1 > geom_height/geom_width){
+						rate = view_w/geom_width;
+						double d_height = (view_h/rate - geom_height)/2;
+						mGeomEnvelope.init(maxx, minx, maxy + d_height, miny - d_height);
+					}
+					else {
+						rate = view_h/geom_height;
+						double d_with  = (view_w/rate - geom_width)/2;
+						mGeomEnvelope.init(maxx + d_with,minx - d_with, maxy, miny);
 					}
 				}
-				// db.open(Environment.getExternalStorageDirectory() +
-				// "/download/test-2.3.sqlite",jsqlite.Constants.SQLITE_OPEN_READONLY);
-				Toast.makeText(getApplicationContext(), "正在加载数据库……",
-						Toast.LENGTH_SHORT).show();
-				db.open("/mnt/sdcard/test-2.3.sqlite",
-						jsqlite.Constants.SQLITE_OPEN_READONLY);
-				Callback cb = new Callback() {
-					@Override
-					public void columns(String[] coldata) {
-						// Log.v(TAG, "Columns: " + Arrays.toString(coldata));
-					}
-
-					@Override
-					public void types(String[] types) {
-						// Log.v(TAG, "Types: " + Arrays.toString(types));
-					}
-
-					@Override
-					public boolean newrow(String[] rowdata) {
-						Log.v(TAG, "Row: " + Arrays.toString(rowdata));
-						String regEx = "POINT\\(([0-9]+\\.[0-9]+)\\s([0-9]+\\.[0-9]+)\\)";
-						String str = Arrays.toString(rowdata);
-						Pattern p = Pattern.compile(regEx);
-						Matcher m = p.matcher(str);
-						boolean rs = m.find();
-						if (rs) {
-							Float X = new Float(m.group(1));
-							Float Y = new Float(m.group(2));
-							GeoPoint gp = new GeoPoint();
-							gp.x = X.floatValue();
-							gp.y = Y.floatValue();
-							gp.name = rowdata[0];
-							pointArrays.add(gp);
-						}
-						return false;
-					}
-				};
-
-				String query = "SELECT name, peoples, AsText(Geometry) from Towns where peoples > 350000";
-				db.exec(query, cb);
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
+				leavel = rate;
+				stmt.close();
 			} catch (Exception e) {
-				e.printStackTrace();
+				Log.e(TAG, e.getMessage());
 			}
 		}
 	}
-
 }
